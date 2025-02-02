4767a35f4ba3da82dbfa438663030710a97ea84e
ViacheslavKuznetsov <155986232+samatwa@users.noreply.github.com>
2025-02-02 11:40:57 +0100

    transactions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import userData from "./userData.json";
 import friends from "./friends.json";
+import transactions from "./transactions.json";
 import FriendList from "./components/FriendList/FriendList.jsx";
 import Profile from "./components/Profile/Profile.jsx";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory.jsx";
 import "./App.css";
 
 const App = () => {
@@ -15,6 +17,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
     </>
   );
 };
